5fe16570283d6d375c25cca29920129fa5ff9bea
Felipe Silva <felipe.dacs@gmail.com>
2020-05-29 22:34:00 -0300

    feat: add estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 declare const config: object;
 
 function App() {
   console.log(config)
   return (
+    <>
     <div>
     </div>
+      <GlobalStyle />
+    </>
   );
 }
 
